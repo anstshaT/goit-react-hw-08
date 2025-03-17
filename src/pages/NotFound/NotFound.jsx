6d78ca4d45e86d51945473726af2bcb6d0492f65
Anastasiia Tabachnikova <nastiatab11@gmail.com>
2025-03-17 13:47:07 +0100

    Add notFound page style

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,5 +1,15 @@
+import { Link } from "react-router";
+import s from "./NotFound.module.css";
+
 const NotFound = () => {
-  return <div>Page not found</div>;
+  return (
+    <div className={s.notFoundDiv}>
+      <p className={s.notFoundP}>Page not found</p>
+      <Link to="/" className={s.notFoundLink}>
+        Home page
+      </Link>
+    </div>
+  );
 };
 
 export default NotFound;
